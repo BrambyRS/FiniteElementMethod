adb881bf39e720fd1781cecab9e681ea0fe0bf78
Ranbir Singh <ranbir1998@gmail.com>
2025-09-11 19:45:58 +0100

    Add nodal value property to mesh

--- src/meshing.rs
+++ src/meshing.rs
@@ -1,14 +1,19 @@
+use std::vec;
+
 pub struct Mesh1D {
     n_elements: usize,
 
     element_size: Vec<f64>,     // Length of each element
     node_coordinates: Vec<f64>, // Position of each node
+
+    pub node_values: Vec<f64>, // Value at each node
 }
 
 impl Mesh1D {
     pub fn uniform_mesh(length: f64, n_elements: usize) -> Self {
         let element_size: Vec<f64> = vec![length / n_elements as f64; n_elements];
         let mut node_coordinates: Vec<f64> = Vec::with_capacity(n_elements + 1);
+        let node_values: Vec<f64> = vec![0.0; n_elements + 1];
         for i in 0..=n_elements {
             node_coordinates.push(i as f64 * element_size[0]);
         }
@@ -16,12 +21,14 @@ impl Mesh1D {
             n_elements,
             element_size,
             node_coordinates,
+            node_values,
         };
     }
 
     pub fn general_mesh(node_coordinates: Vec<f64>) -> Self {
         let n_elements = node_coordinates.len() - 1;
         let mut element_size: Vec<f64> = Vec::with_capacity(n_elements);
+        let node_values: Vec<f64> = vec![0.0; n_elements + 1];
         for i in 0..n_elements {
             element_size.push(node_coordinates[i + 1] - node_coordinates[i]);
         }
@@ -29,6 +36,7 @@ impl Mesh1D {
             n_elements,
             element_size,
             node_coordinates,
+            node_values,
         };
     }
 
@@ -43,6 +51,10 @@ impl Mesh1D {
     pub fn get_node_coordinates(&self, node: usize) -> Option<f64> {
         self.node_coordinates.get(node).copied()
     }
+
+    pub fn get_node_values(&self, node: usize) -> Option<f64> {
+        self.node_values.get(node).copied()
+    }
 }
 
 #[cfg(test)]
@@ -55,6 +67,7 @@ mod tests {
         assert_eq!(mesh.n_elements, 5);
         assert_eq!(mesh.element_size, vec![2.0, 2.0, 2.0, 2.0, 2.0]);
         assert_eq!(mesh.node_coordinates, vec![0.0, 2.0, 4.0, 6.0, 8.0, 10.0]);
+        assert_eq!(mesh.node_values, vec![0.0; 6]);
     }
 
     #[test]
@@ -63,6 +76,7 @@ mod tests {
         assert_eq!(mesh.n_elements, 5);
         assert_eq!(mesh.element_size, vec![2.0, 2.0, 2.0, 2.0, 2.0]);
         assert_eq!(mesh.node_coordinates, vec![0.0, 2.0, 4.0, 6.0, 8.0, 10.0]);
+        assert_eq!(mesh.node_values, vec![0.0; 6]);
     }
 
     #[test]
@@ -71,5 +85,6 @@ mod tests {
         assert_eq!(mesh.get_n_elements(), 5);
         assert_eq!(mesh.get_element_size(0), Some(2.0));
         assert_eq!(mesh.get_node_coordinates(0), Some(0.0));
+        assert_eq!(mesh.get_node_values(0), Some(0.0));
     }
 }
