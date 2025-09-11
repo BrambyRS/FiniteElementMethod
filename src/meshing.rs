20eb80aeadfd8e7ae268ec0ddce9a74f96e7f9cb
Ranbir Singh <ranbir1998@gmail.com>
2025-09-11 19:28:49 +0100

    Make functions public

--- src/meshing.rs
+++ src/meshing.rs
@@ -1,4 +1,4 @@
-struct Mesh1D {
+pub struct Mesh1D {
     n_elements: usize,
 
     element_size: Vec<f64>,     // Length of each element
@@ -6,7 +6,7 @@ struct Mesh1D {
 }
 
 impl Mesh1D {
-    fn uniform_mesh(length: f64, n_elements: usize) -> Self {
+    pub fn uniform_mesh(length: f64, n_elements: usize) -> Self {
         let element_size: Vec<f64> = vec![length / n_elements as f64; n_elements];
         let mut node_coordinates: Vec<f64> = Vec::with_capacity(n_elements + 1);
         for i in 0..=n_elements {
@@ -19,7 +19,7 @@ impl Mesh1D {
         };
     }
 
-    fn general_mesh(node_coordinates: Vec<f64>) -> Self {
+    pub fn general_mesh(node_coordinates: Vec<f64>) -> Self {
         let n_elements = node_coordinates.len() - 1;
         let mut element_size: Vec<f64> = Vec::with_capacity(n_elements);
         for i in 0..n_elements {
@@ -30,6 +30,18 @@ impl Mesh1D {
             element_size,
             node_coordinates,
         };
+    }
+
+    pub fn get_n_elements(&self) -> usize {
+        return self.n_elements;
+    }
+
+    pub fn get_element_size(&self, element: usize) -> Option<f64> {
+        self.element_size.get(element).copied()
+    }
+
+    pub fn get_node_coordinates(&self, node: usize) -> Option<f64> {
+        self.node_coordinates.get(node).copied()
     }
 }
 
@@ -51,5 +63,13 @@ mod tests {
         assert_eq!(mesh.n_elements, 5);
         assert_eq!(mesh.element_size, vec![2.0, 2.0, 2.0, 2.0, 2.0]);
         assert_eq!(mesh.node_coordinates, vec![0.0, 2.0, 4.0, 6.0, 8.0, 10.0]);
+    }
+
+    #[test]
+    fn test_getters() {
+        let mesh = Mesh1D::uniform_mesh(10.0, 5);
+        assert_eq!(mesh.get_n_elements(), 5);
+        assert_eq!(mesh.get_element_size(0), Some(2.0));
+        assert_eq!(mesh.get_node_coordinates(0), Some(0.0));
     }
 }
