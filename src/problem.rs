2f0cc2a9ca72c797f8b09e40061c2d1d7122264c
Ranbir Singh <ranbir1998@gmail.com>
2025-09-12 19:31:41 +0100

    Futher partial implementation of problem struct

--- src/problem.rs
+++ src/problem.rs
@@ -3,6 +3,8 @@ Only allows for problems with a Dirichlet boundary condition at the left boundar
 and a Neumann boundary condition at the right boundary
 */
 
+use crate::lin_alg;
+use crate::lin_alg::mat;
 use crate::meshing::Mesh1D;
 
 pub struct Problem1D {
@@ -20,7 +22,27 @@ impl Problem1D {
         };
     }
 
-    pub fn solve(&self) {
-        // TODO: Implement solve
+    pub fn solve(&mut self) {
+        let (k, f) = self.construct_matrices();
+        let d: mat::Matrix<f64> = lin_alg::lin_solve(&k, &f);
+
+        // Apply nodal values to mesh
+        for i in 0..self.mesh.get_n_elements() + 1 {
+            self.mesh.node_values[i] = d.get(i, 0);
+        }
+    }
+
+    fn construct_matrices(&self) -> (mat::Matrix<f64>, mat::Matrix<f64>) {
+        // Stiffness matrix
+        let mut k: mat::Matrix<f64> = mat::Matrix::new((
+            self.mesh.get_n_elements() + 1,
+            self.mesh.get_n_elements() + 1,
+        ));
+        // Force matrix
+        let mut f: mat::Matrix<f64> = mat::Matrix::new((self.mesh.get_n_elements() + 1, 1));
+
+        // TODO: Implement matrix assembly
+
+        return (k, f);
     }
 }
