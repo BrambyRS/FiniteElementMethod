d9d8601dc3a556e77a9c7b0430a5891a1ca341ca
Ranbir Singh <rsingh@mercedesamgf1.com>
2025-10-03 10:46:15 +0100

    Add integration function to quadrature struct

--- src/maths/numerics/quadrature.rs
+++ src/maths/numerics/quadrature.rs
@@ -1,13 +1,14 @@
 pub struct GaussLegendre {
+    pub order: usize,
     pub nodes: Vec<f64>,
     pub weights: Vec<f64>,
 }
 
 impl GaussLegendre {
-    pub fn new(interval: (f64, f64), order: usize) -> Self {
+    pub fn new(order: usize) -> Self {
         // Get nodes and weights in bi-unit domain [-1, 1]
         // TODO: Generalise for higher orders
-        let (nodes_biunit, weights) = match order {
+        let (nodes, weights) = match order {
             1 => (vec![0.0], vec![2.0]),
             2 => {
                 let term: f64 = 1.0 / 3f64.sqrt();
@@ -23,16 +24,25 @@ impl GaussLegendre {
             _ => panic!("Order not implemented"),
         };
 
-        // Map nodes to desired interval [a, b]
-        let a: f64 = interval.0;
-        let b: f64 = interval.1;
-        let mut nodes: Vec<f64> = Vec::with_capacity(nodes_biunit.len());
-        for &xi in nodes_biunit.iter() {
-            let x_mapped: f64 = 0.5 * (b - a) * xi + 0.5 * (a + b);
-            nodes.push(x_mapped);
-        }
+        return Self {
+            order,
+            nodes,
+            weights,
+        };
+    }
 
-        return Self { nodes, weights };
+    pub fn integrate(&self, f: fn(f64) -> f64, a: f64, b: f64) -> f64 {
+        assert!(
+            a < b,
+            "Invalid integration limits. a = {a} must be less than b = {b}."
+        );
+
+        let mut integral: f64 = 0.0;
+        for i in 0..self.order {
+            let x: f64 = ((b - a) * self.nodes[i] + (a + b)) * 0.5;
+            integral += self.weights[i] * f(x);
+        }
+        return integral * (b - a) * 0.5;
     }
 }
 
@@ -42,16 +52,24 @@ mod tests {
 
     #[test]
     fn test_gauss_legendre_order_1() {
-        let gl = GaussLegendre::new((-2.0, 4.0), 1);
-        assert_eq!(gl.nodes, vec![1.0]);
+        let gl = GaussLegendre::new(1);
+        assert_eq!(gl.nodes, vec![0.0]);
         assert_eq!(gl.weights, vec![2.0]);
+
+        // Test integration of f(x) = 2x+1 over [2, 4]
+        let expected_integral = 14.0;
+        let integral = gl.integrate(|x: f64| 2.0 * x + 1.0, 2.0, 4.0);
+        assert!(
+            (integral - expected_integral).abs() < 1e-12,
+            "Integral does not match expected value. Got {integral}, expected {expected_integral}"
+        );
     }
 
     #[test]
     fn test_gauss_legendre_order_2() {
-        let gl = GaussLegendre::new((1.0, 3.0), 2);
+        let gl = GaussLegendre::new(2);
         let term = 1.0 / 3f64.sqrt();
-        let expected_nodes = vec![2.0 - term, 2.0 + term];
+        let expected_nodes = vec![-term, term];
         assert!(
             gl.nodes
                 .iter()
@@ -60,13 +78,25 @@ mod tests {
             "Nodes do not match expected values"
         );
         assert_eq!(gl.weights, vec![1.0, 1.0]);
+
+        // Test integration of f(x) = x^3-3x^2+3x-1 over [-1, 2]
+        let expected_integral: f64 = -3.75;
+        let integral = gl.integrate(
+            |x: f64| x.powi(3) - 3.0 * x.powi(2) + 3.0 * x - 1.0,
+            -1.0,
+            2.0,
+        );
+        assert!(
+            (integral - expected_integral).abs() < 1e-12,
+            "Integral does not match expected value. Got {integral}, expected {expected_integral}",
+        );
     }
 
     #[test]
     fn test_gauss_legendre_order_3() {
-        let gl = GaussLegendre::new((-1.0, 5.0), 3);
+        let gl = GaussLegendre::new(3);
         let term: f64 = (3f64 / 5f64).sqrt();
-        let expected_nodes = vec![2.0 - 3.0 * term, 2.0, 2.0 + 3.0 * term];
+        let expected_nodes = vec![-term, 0.0, term];
         assert!(
             gl.nodes
                 .iter()
@@ -75,5 +105,27 @@ mod tests {
             "Nodes do not match expected values"
         );
         assert_eq!(gl.weights, vec![5.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0]);
+
+        // Test integration of f(x) = 6x^5-3x^3-3x^2+2 over [-3, 2]
+        let expected_integral: f64 = -641.25;
+        let integral = gl.integrate(
+            |x: f64| 6.0 * x.powi(5) - 3.0 * x.powi(3) - 3.0 * x.powi(2) + 2.0,
+            -3.0,
+            2.0,
+        );
+        assert!(
+            (integral - expected_integral).abs() < 1e-12,
+            "Integral does not match expected value. Got {integral}, expected {expected_integral}",
+        );
+    }
+
+    #[test]
+    fn test_faulty_interval() {
+        let gl = GaussLegendre::new(2);
+        let result: Result<(), Box<dyn std::any::Any + Send + 'static>> =
+            std::panic::catch_unwind(|| {
+                gl.integrate(|x: f64| x.powi(2), 2.0, 1.0);
+            });
+        assert!(result.is_err(), "Expected panic for invalid interval");
     }
 }
